6448989e982dd1b0b4c7a2cf9b2d154a40884b15
Denis Zangarov <gamingdeathadder98@gmail.com>
2024-01-30 01:30:06 +0200

    add goroutines advanced wait

---
+++ 07 - goroutines-wait-groups-advanced/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Number struct {
+	x int
+	y int
+	xMu sync.Mutex	// used for locking X value when simultaneously writing to it
+	yMu sync.Mutex  // used for locking Y value when simultaneously writing to it
+}
+
+func NewNumber(x int, y int) *Number {
+	return &Number{x,y, sync.Mutex{}, sync.Mutex{}}
+}
+
+func (c *Number) AddX(x int){
+	c.xMu.Lock()
+	defer c.xMu.Unlock()
+	c.x+=x
+}
+
+func (c *Number) AddY(y int){
+	c.yMu.Lock()
+	defer c.yMu.Unlock()
+	c.y+=y
+}
+
+func (c* Number) Print(){
+	fmt.Printf("x: %v\ny: %v\n", c.x, c.y)
+}
+
+func (c* Number) AddSimultaneously(x int, y int){
+	wg := sync.WaitGroup{}
+	for i:=0; i<100; i++{
+		wg.Add(1)
+		go func(){
+			c.AddX(x)
+			defer wg.Done()
+		}()
+
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			c.AddY(y)
+		}()
+	}
+
+	wg.Wait()
+	
+}
+
+func main(){
+	fmt.Println("Cool")
+	number:=NewNumber(5,5)
+	number.AddX(5)
+	number.Print()
+	number.AddSimultaneously(5, 10)
+	number.Print()
+}
